1fcc730ee37d38e3650f59de95882fde0ffb2d43
gostek223 <pingwin.gawronek@gmail.com>
2026-03-17 19:11:21 +0100

    Added python loading and testing script

---
+++ load_model.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+import sklearn
+from sklearn.datasets import load_iris
+with open('model_iris.pkl', 'rb') as f:
+    model = pickle.load(f)
+test=np.array([[5.1, 3.5, 1.4, 0.2]])
+wynik_testu= model.predict(test)
+nazwy = ['setosa', 'versicolor', 'virginica']
+nazwa_pred = nazwy[wynik_testu[0]]
+print(f"dla kwiata o parametrach {test} model wskazał {nazwa_pred}")
